8700e669ba60dff88e01231cb26cbd2fa17c7711
Tor <torrobinson@gmail.com>
2020-08-13 20:30:29 -0600

    Update github2synology.sh

--- github2synology.sh
+++ github2synology.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # token from https://github.com/settings/tokens
 OAUTH_TOKEN="[YOUR TOKEN HERE]"
-BACKUP_PATH="/volume1/Sourcecode/GitHub"
+BACKUP_PATH="/volume1/sourcecode/GitHub" # Feel free to change this to anything. `volume1` is the root volume.
 API_URL="https://api.github.com/user/repos?type=owner&per_page=100&page=1"
 
 echo "Fetching from ${API_URL}"
